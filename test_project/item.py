eaa7f5fc83f051637db4cec9d87a4e8ed80fe5e2
Kiril <kirilvasiljev1@gmail.com>
2026-05-21 18:44:53 +0300

    Item Class created

---
+++ test_project/item.py
@@ -0,0 +1,19 @@
+from abc import ABC, abstractmethod
+
+class Item(ABC):
+    def __init__(self, title, author, name):
+        super().__init__()
+        self.title = title
+        self.author = author
+        self.name = name
+        self.__is_checked_out = False
+
+    def get_age(self):
+        return self.__is_checked_out
+    
+    def set_age(self, __is_checked_out):
+        self.__is_checked_out = __is_checked_out
+
+    @abstractmethod
+    def get_details(self):
+        print(self.title, self.author, self.name)
